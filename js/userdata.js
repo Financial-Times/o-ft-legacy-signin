54b184e9dbe8335060abb32114c3c070aa3093a8
Rhys Evans <rhys.evans@ft.com>
2014-01-31 12:28:26 +0000

    updated to use either FT_User or FT_Remember cookies

--- js/userdata.js
+++ js/userdata.js
@@ -1,7 +1,7 @@
 var cookie = require('o-cookies'),
-    firstName = cookie.getParam('FT_Remember', 'FNAME'),
-    lastName = cookie.getParam('FT_Remember', 'LNAME'),
-    email = cookie.getParam('FT_Remember', 'EMAIL');
+    firstName = cookie.getParam('FT_Remember', 'FNAME') || cookie.getParam('FT_User', 'FNAME'),
+    lastName = cookie.getParam('FT_Remember', 'LNAME') || cookie.getParam('FT_User', 'LNAME'),
+    email = cookie.getParam('FT_Remember', 'EMAIL') || cookie.getParam('FT_User', 'EMAIL');
  
 module.exports = {
     getName: function () {
