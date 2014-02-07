e82ecc1846a849833213331f081d78ceb07b3e4c
Rhys Evans <rhys.evans@ft.com>
2014-02-07 12:15:15 +0000

    preventing click throughs to barrier when already signed in

--- main.js
+++ main.js
@@ -11,6 +11,9 @@ if (userEmail) {
     $el.addClass('o-ft-signin--logged-in');
     el.title = 'Logged in as ' + userEmail;
     $el.data('o-dialog__trigger', null).removeAttr('data-o-dialog__trigger');
+    $el.on('click', function (ev) {
+        ev.preventDefault();
+    });
 } else {
     $el.data('o-dialog__trigger').onAfterRender = function () {
         $('.o-ft-signin__location-field')[0].value = window.location.href;
